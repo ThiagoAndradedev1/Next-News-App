1108b1f36a95f6608c065f9c20c35a1eee719364
Thiago <thiago.andrade.dev1@gmail.com>
2021-05-11 10:06:12 -0300

    Cria página de about

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import Layout from "@components/Layout";
+import React from "react";
 
 const about = () => {
-    return (
-        <div>
-            <h1>About</h1>
-        </div>
-    )
-}
+  return (
+    <Layout title="Next News - Sobre">
+      <h1>About</h1>
+    </Layout>
+  );
+};
 
-export default about
+export default about;
