0c1b5fe422a84e80bfab14a1f24df25efbaf212b
Thiago <thiago.andrade.dev1@gmail.com>
2021-05-11 10:09:07 -0300

    Altera estilo da navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,47 +1,65 @@
 import React from "react";
 
+import Image from "next/image";
+import Link from "next/link";
+
 const Navbar = () => {
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            News App
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarColor01"
-            aria-controls="navbarColor01"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+    <header className="p-3 text-white" style={{ backgroundColor: "#463763" }}>
+      <div className="container">
+        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+          <a
+            href="/"
+            className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
           >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+            {/* <Image src="/images/logo.png" width="40" height="32" /> */}
+          </a>
 
-          <div className="collapse navbar-collapse" id="navbarColor01">
-            <ul className="navbar-nav me-auto">
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  About
-                </a>
-              </li>
-            </ul>
-            <form className="d-flex">
-              <input
-                className="form-control me-sm-2"
-                type="text"
-                placeholder="Search"
-              />
-              <button className="btn btn-secondary my-2 my-sm-0" type="submit">
-                Search
-              </button>
-            </form>
+          <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+            <li>
+              <Link href="/">
+                <a className="nav-link px-2 text-white">Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/news">
+                <a className="nav-link px-2 text-white">Nóticias</a>
+              </Link>
+            </li>
+            {/* <li>
+            <a href="#" className="nav-link px-2 text-white">
+              Add a Guide
+            </a>
+          </li> */}
+            <li>
+              <Link href="/about">
+                <a className="nav-link px-2 text-white">About</a>
+              </Link>
+            </li>
+          </ul>
+          {/* 
+        <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+          <input
+            type="search"
+            className="form-control form-control-dark"
+            placeholder="Search..."
+          />
+        </form> */}
+
+          <div className="text-end">
+            <Link href="/guides/add">
+              <a className="btn btn-warning">Add a Guide</a>
+            </Link>
+            {/* <button type="button" className="btn btn-outline-light me-2">
+            Login
+          </button>
+          <button type="button" className="btn btn-warning">
+            Sign-up
+          </button> */}
           </div>
         </div>
-      </nav>
-    </div>
+      </div>
+    </header>
   );
 };
 
